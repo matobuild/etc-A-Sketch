ab31502f6a729d8adcd03ba8d999dc366bdff6ac
Mato <Kittawat@icloud.com>
2022-11-05 19:52:21 +0800

    create button to create grid

--- index.js
+++ index.js
@@ -1,44 +1,69 @@
-const containerWidth = 960
-const containerHeight = 960
+const containerWidth = 960;
+const containerHeight = 960;
 
-const container = document.querySelector ('.container');
-container.style.width = containerWidth + 'px';
-container.style.height = containerHeight + 'px';
-container.style.backgroundColor = 'black';
-container.style.border = '20px solid black';
+createGrid();
+
+function createGrid(withNum = 16 * 16) {
+    let numberOfGrid = withNum;
+    const numberOfColumn = Math.sqrt(numberOfGrid);
+    const numberOfRow = numberOfColumn;
+  
+    const container = document.querySelector(".container");
+    container.style.width = containerWidth + "px";
+    container.style.height = containerHeight + "px";
+    container.style.backgroundColor = "black";
+    container.style.border = "20px solid black";
+  
+    const grid = document.createElement("div");
+    grid.classList.add("grid");
+    grid.style.backgroundColor = "green";
+    grid.style.width = containerWidth / numberOfColumn - 2 + "px";
+    grid.style.height = containerHeight / numberOfRow - 2 + "px";
+    grid.style.maxWidth = 100;
+    grid.style.border = "1px solid";
+    grid.style.flexShrink = "0";
+  
+    const row = document.createElement("div");
+    row.classList.add("row");
+    row.style.backgroundColor = "red";
+    row.style.width = containerWidth + "px";
+    row.style.height = containerHeight / numberOfRow + "px";
+    row.style.maxWidth = 100;
+    row.style.flexShrink = "0";
+    row.style.display = "flex";
+  
+    for (let i = 0; i < numberOfColumn; i++) {
+      //put grid in row
+      const gridClone = grid.cloneNode(true);
+      row.appendChild(gridClone);
+    }
+  
+    for (let i = 0; i < numberOfRow; i++) {
+      //created rows
+      const rowClone = row.cloneNode(true);
+      container.appendChild(rowClone);
+    }
+    const gridList = document.querySelectorAll(".grid");
+    gridList.forEach((grid) => {
+      grid.addEventListener("mouseover", () => {
+        grid.style.backgroundColor = "blue";
+      });
+    });
+    
+  
+  }
 
 
-const grid = document.createElement('div');
-grid.classList.add ('grid');
-grid.style.backgroundColor = 'green';
-grid.style.width = (containerWidth/4 - 2) + 'px';
-grid.style.height = (containerHeight/4 -2) + 'px';
-grid.style.maxWidth = 100;
-grid.style.border = '1px solid';
-grid.style.flexShrink = '0';
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  numberOfGridPerSide = prompt("How many squares per side for the new grid?");
+  createGrid(numberOfGridPerSide * numberOfGridPerSide);
 
-let numberOfGrid = 4;
-let numberOfRow = numberOfGrid;
+});
 
 
-const row = document.createElement('div');
-row.classList.add ('row');
-row.style.backgroundColor = 'red';
-row.style.width = containerWidth + 'px';
-row.style.height = containerHeight/4 + 'px';
-row.style.maxWidth = 100;
-row.style.flexShrink = '0';
-row.style.display = 'flex';
 
+function removeGrid() {
+    container.remove();
+}
 
-for (let i = 0; i < numberOfGrid; i++) {
-    //put grid in row
-    const gridClone = grid.cloneNode(true);
-    row.appendChild(gridClone);
- };
-
- for (let i = 0; i < numberOfRow; i++) {
-    //created rows
-    const rowClone = row.cloneNode(true);
-    container.appendChild(rowClone);   
-};
