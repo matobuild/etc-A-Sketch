a54113ea7bfa62bd9fa3d28268879386772f5f4c
Mato <Kittawat@icloud.com>
2022-11-05 14:58:03 +0800

    fix border issue

--- index.js
+++ index.js
@@ -4,17 +4,18 @@ const containerHeight = 960
 const container = document.querySelector ('.container');
 container.style.width = containerWidth + 'px';
 container.style.height = containerHeight + 'px';
+container.style.backgroundColor = 'black';
+container.style.border = '20px solid black';
 
 
 const grid = document.createElement('div');
 grid.classList.add ('grid');
 grid.style.backgroundColor = 'green';
-grid.style.width = containerWidth/4 + 'px';
-grid.style.height = containerHeight/4 + 'px';
+grid.style.width = (containerWidth/4 - 2) + 'px';
+grid.style.height = (containerHeight/4 -2) + 'px';
 grid.style.maxWidth = 100;
 grid.style.border = '1px solid';
 grid.style.flexShrink = '0';
-
 
 let numberOfGrid = 4;
 let numberOfRow = numberOfGrid;
@@ -26,10 +27,8 @@ row.style.backgroundColor = 'red';
 row.style.width = containerWidth + 'px';
 row.style.height = containerHeight/4 + 'px';
 row.style.maxWidth = 100;
-row.style.border = '1px solid';
 row.style.flexShrink = '0';
 row.style.display = 'flex';
-
 
 
 for (let i = 0; i < numberOfGrid; i++) {
@@ -38,13 +37,8 @@ for (let i = 0; i < numberOfGrid; i++) {
     row.appendChild(gridClone);
  };
 
- for (let i = 0; i < 4; i++) {
+ for (let i = 0; i < numberOfRow; i++) {
     //created rows
     const rowClone = row.cloneNode(true);
     container.appendChild(rowClone);   
 };
-
-
-//NEED TO FIX BORDER ISSUE
-
-
